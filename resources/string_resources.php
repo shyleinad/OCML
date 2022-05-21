6886965fb4747290565d9336a43697e69a9a0a58
Túri Dániel <turi.daniel@protonmail.com>
2022-05-21 16:37:26 +0200

    font awesome bugfix

--- resources/string_resources.php
+++ resources/string_resources.php
@@ -23,7 +23,7 @@
  $string_bootstrap_js_route="./dependencies/bootstrap-5.1.3-dist/js/bootstrap.min.js";
  $string_jquery_route="./dependencies/jquery-3.6.0.min.js";
  $string_menu_route="./view/menu.php";
- $string_fontawesome_css_route="./dependencies/fontawesome-free-6.1.1-web/css/fontawesome.min.css";
+ $string_fontawesome_css_route="./dependencies/fontawesome-free-6.1.1-web/css/all.min.css";
  $string_controller_route="./controller/";
  $string_controller_modals_route="./controller/modals/";
  $string_view_modals_route="./view/modals/";
