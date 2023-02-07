9c5a46fbd0de640f4e00cc1bbe59f13484d44591
shyleinad <turi.daniel@protonmail.com>
2023-02-07 15:04:23 +0100

    php string resource organizing

--- resources/string_resources.php
+++ resources/string_resources.php
@@ -19,17 +19,23 @@
  $string_meta_title_hu="Online gépjármű szevizkönyv";
 
  //elérési utak
+ //dependencies
  $string_bootstrap_css_route="./dependencies/bootstrap-5.1.3-dist/css/bootstrap.min.css";
  $string_bootstrap_js_route="./dependencies/bootstrap-5.1.3-dist/js/bootstrap.min.js";
  $string_jquery_route="./dependencies/jquery-3.6.0.min.js";
- $string_menu_route="./view/menu.php";
  $string_fontawesome_css_route="./dependencies/fontawesome-free-6.1.1-web/css/all.min.css";
- $string_controller_route="./controller/";
- $string_controller_modals_route="./controller/modals/";
+ //view
+ $string_menu_route="./view/menu.php";
  $string_view_modals_route="./view/modals/";
- $string_resources_stringresourcesjs_route="./resources/string_resources.js";
- $string_common_tokengen_route="./common/tokengen.php";
  $string_view_top_hu_route="./view/top_hu.php";
  $string_view_mid_route="./view/mid.php";
  $string_view_bottom_route="./view/bottom.php";
+ //controller
+ $string_controller_route="./controller/";
+ $string_controller_modals_route="./controller/modals/";
+ $string_controller_user="./controller/user.php";
+ //resources
+ $string_resources_stringresourcesjs_route="./resources/string_resources.js";
+ //common
+ $string_common_tokengen_route="./common/tokengen.php";
  ?>
