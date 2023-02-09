9cce25a403ddf49ef0ad43780cf40eebf4df6045
shyleinad <turi.daniel@protonmail.com>
2023-02-09 11:14:09 +0100

    index.php put in try-catch

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php
-  session_start();
-  require_once('./resources/string_resources.php');
-  require_once($string_common_tokengen_route);
-  generateToken();
-  //require_once('common/language.php');
-  //require_once(classLanguage::stringSetHtmlTop());
-  require_once($string_view_top_hu_route);
-  require_once($string_view_mid_route);
-  require_once($string_view_bottom_route);
+  try{
+    session_start();
+    require_once('./resources/string_resources.php');
+    require_once('./db_config/connect.php');
+    require_once($string_common_tokengen_route);
+    generateToken();
+    //require_once('common/language.php');
+    //require_once(classLanguage::stringSetHtmlTop());
+    require_once($string_view_top_hu_route);
+    require_once($string_view_mid_route);
+    require_once($string_view_bottom_route);
+  }
+  catch(Exception $e){
+    echo($e); //később átírni logolásra
+  }
 ?>
