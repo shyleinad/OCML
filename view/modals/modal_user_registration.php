8879ef7caed06b6d146901e183a765a57990b86d
shyleinad <turi.daniel@protonmail.com>
2023-03-01 13:19:48 +0100

    added password confirm, inputs required on html side

--- view/modals/modal_user_registration.php
+++ view/modals/modal_user_registration.php
@@ -10,31 +10,30 @@
         <form>
           <div class="mb-3">
             <label for="input_user_reg_email" class="form-label">Email cím</label>
-            <input type="email" class="form-control" id="input_user_reg_email">
+            <input type="email" class="form-control" id="input_user_reg_email" required>
             <!--<div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>-->
           </div>
           <div class="mb-3">
             <label for="input_user_reg_name" class="form-label">Teljes név</label>
-            <input type="text" class="form-control" id="input_user_reg_name">
+            <input type="text" class="form-control" id="input_user_reg_name" required>
           </div>
           <div class="mb-3">
             <label for="input_user_reg_borndate" class="form-label">Születési idő</label>
-            <input type="date" class="form-control" id="input_user_reg_borndate" min="1920-01-01" max="<?php echo(date("Y-m-d")); ?>">
-            <!--<label for="select_user_reg_bornyear" class="form-label">Születési év</label>
-            <select id="select_user_reg_bornyear" class="form-select">
-              <option>Select</option>
-            </select>-->
+            <input type="date" class="form-control" id="input_user_reg_borndate" min="1920-01-01" max="<?php echo(date("Y-m-d")); ?>" required>
           </div>
           <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Jelszó</label>
-            <input type="password" class="form-control" id="input_user_reg_pswd">
+            <label for="input_user_reg_pswd" class="form-label">Jelszó</label>
+            <input type="password" class="form-control" id="input_user_reg_pswd" required>
           </div>
-          <!--<button type="submit" class="btn btn-primary">Submit</button>-->
+          <div class="mb-3">
+            <label for="input_user_reg_pswd_confirm" class="form-label">Jelszó megerősítése</label>
+            <input type="password" class="form-control" id="input_user_reg_pswd_confirm" required>
+          </div>
         </form>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Mégse</button>
-        <button type="button" class="btn btn-primary" id="button_user_reg_send">Regisztráció</button>
+        <button type="submit" class="btn btn-primary" id="button_user_reg_send">Regisztráció</button>
       </div>
     </div>
   </div>
