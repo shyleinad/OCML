6f34c2c613abd4d560381aa56e86c948b0f889bf
shyleinad <turi.daniel@protonmail.com>
2023-03-07 14:01:40 +0100

    beautify

--- resources/string_resources.js
+++ resources/string_resources.js
@@ -7,18 +7,23 @@ $string_fontawesome_css_route="dependencies/fontawesome-free-6.1.1-web/css/fonta
 $string_controller_route="./controller/";
 $string_controller_modals_route="./controller/modals/";
 $string_view_modals_route="./view/modals/";*/
+
 //models
-var string_model_user_route="./model/user.php";
+var string_model_user_route = "./model/user.php";
+
 //controllers
-var string_controller_user="./controller/user.php";
+
+var string_controller_user = "./controller/user.php";
+
 //html input ids
 ///reg
-var string_input_user_reg_email="input_user_reg_email";
-var string_input_user_reg_name="input_user_reg_name";
-var string_input_user_reg_borndate="input_user_reg_borndate";
-var string_input_user_reg_pswd="input_user_reg_pswd";
-var string_input_user_reg_pswdconfirm="input_user_reg_pswd_confirm";
+var string_input_user_reg_email = "input_user_reg_email";
+var string_input_user_reg_name = "input_user_reg_name";
+var string_input_user_reg_borndate = "input_user_reg_borndate";
+var string_input_user_reg_pswd = "input_user_reg_pswd";
+var string_input_user_reg_pswdconfirm = "input_user_reg_pswd_confirm";
+
 //regexes
-var email_regex=/^((([a-z0-9\-(_)(.)])+)(@)(([a-z0-9(_)\-]+\.)+)([a-z]{2,4}))|(\-)$"/;
-var name_regex=/^(([AÁBCDEÉFGHIÍJKLMNOÓÖŐPQRSTUÚÜŰVWXYZ][aábcdeéfghiíjklmnoóöőpqrstuúüűvwxyz]+((\-)|(\ ))?)+)(\ [AÁBCDEÉFGHIÍJKLMNOÓÖŐPQRSTUÚÜŰVWXYZ][aábcdeéfghiíjklmnoóöőpqrstuúüűvwxyz]+)$/;
-var pswd_regex=/^(?=.*?[AÁBCDEÉFGHIÍJKLMNOÓÖŐPQRSTUÚÜŰVWXYZ])(?=.*?[aábcdeéfghiíjklmnoóöőpqrstuúüűvwxyz])(?=.*?[0-9])(?=.*?[§\'\"\+\!\%\/\=\(\)\~\ˇ\^\˘\°\˛\`\˙\´\˝\¨\¸\\\|\€\÷\×\[\]\$\¤\<\>\#\&\@\{\}\;\>\*\,\.\-]).{8,100}$/;
+var email_regex = /^((([a-z0-9\-(_)(.)])+)(@)(([a-z0-9(_)\-]+\.)+)([a-z]{2,4}))|(\-)$"/;
+var name_regex = /^(([AÁBCDEÉFGHIÍJKLMNOÓÖŐPQRSTUÚÜŰVWXYZ][aábcdeéfghiíjklmnoóöőpqrstuúüűvwxyz]+((\-)|(\ ))?)+)(\ [AÁBCDEÉFGHIÍJKLMNOÓÖŐPQRSTUÚÜŰVWXYZ][aábcdeéfghiíjklmnoóöőpqrstuúüűvwxyz]+)$/;
+var pswd_regex = /^(?=.*?[AÁBCDEÉFGHIÍJKLMNOÓÖŐPQRSTUÚÜŰVWXYZ])(?=.*?[aábcdeéfghiíjklmnoóöőpqrstuúüűvwxyz])(?=.*?[0-9])(?=.*?[§\'\"\+\!\%\/\=\(\)\~\ˇ\^\˘\°\˛\`\˙\´\˝\¨\¸\\\|\€\÷\×\[\]\$\¤\<\>\#\&\@\{\}\;\>\*\,\.\-]).{8,100}$/;
